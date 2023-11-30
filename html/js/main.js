18581b566425a515ed07fce724c8593acaf52e55
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-11-30 12:32:18 +0500

    header footer the end

--- html/js/main.js
+++ html/js/main.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+let burger_btn = document.querySelector('.burger_btn');
+let menu = document.querySelector('.menu');
+let menu_close = document.querySelector('.menu_close');
+
+burger_btn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+menu_close.addEventListener('click', () => {
+    menu.classList.remove('active')
+})
