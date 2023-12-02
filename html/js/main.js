8d6c7811b3de854b9098c27da81691ce2489e716
Mavlon4658 <mavlon4658@gmail.com>
2023-12-02 19:37:29 +0500

    rates is ready

--- html/js/main.js
+++ html/js/main.js
@@ -9,3 +9,51 @@ burger_btn.addEventListener('click', () => {
 menu_close.addEventListener('click', () => {
     menu.classList.remove('active')
 })
+
+let init = false;
+let example_card_1;
+let example = document.querySelector('#examples')
+
+if (example) {
+    function swiperCard() {
+        if (window.innerWidth <= 992) {
+            if (!init) {
+                init = true;
+                example_card_1 = new Swiper("#examples .card_1", {
+                    slidesPerView: 1.1,
+                    centeredSlides: true,
+                    spaceBetween: 11,
+                    loop: true,
+                    pagination: {
+                        el: ".example_card_1_pagination",
+                        clickable: true,
+                    },
+                    breakpoints: {
+                        500: {
+                            slidesPerView: 2,
+                        }
+                    }
+                });
+            }
+        } else if (init) {
+            example_card_1.destroy();
+            init = false;
+        }
+    }
+    swiperCard();
+    window.addEventListener("resize", swiperCard);
+
+    let example_card_2_wrappers = document.querySelectorAll('#examples .card_wrapper .card_item');
+    example_card_2_wrappers.forEach(item => {
+        let slide = item.querySelector('.card_2')
+        let paginate = item.querySelector('.example_card_2_pagination')
+        let card_2 = new Swiper(slide, {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            pagination: {
+                el: paginate,
+                clickable: true,
+            },
+        });
+    })
+}
